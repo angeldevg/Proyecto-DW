289b060b95cbf570b463ce88ed25d8fde00a9e8b
Fernando Sicay <fsicayc@miumg.edu.gt>
2020-10-21 22:54:17 -0600

    Fernando MenuDAO

---
+++ src/main/java/gt/edu/umg/desarrollo/proyecto/Model/MenuDao.java
@@ -0,0 +1,43 @@
+package gt.edu.umg.desarrollo.proyecto.Model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+public class MenuDao implements IMenuDao{
+
+    Conexion conexion;
+    ResultSet consulta;
+
+    
+    //Menu Acceso a Datos
+    @Override
+    public HashMap ReadMEnu() {
+
+        HashMap<String, String> drop = new HashMap();
+
+        try {
+            conexion = new Conexion();
+
+            String query = "select ruta, nombre from menu;";
+
+            conexion.OpenConnection();
+
+            consulta = conexion.conexionBD.createStatement().executeQuery(query);
+
+            while (consulta.next()) {
+
+                drop.put(consulta.getString("ruta"), consulta.getString("nombre"));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }finally{
+            conexion.Closeconnection();
+        }
+
+        return drop;
+    }
+
+
+}
