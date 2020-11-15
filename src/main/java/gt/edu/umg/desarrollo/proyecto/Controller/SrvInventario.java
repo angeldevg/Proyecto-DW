c529b8325cca96aeba65b07e0c647d70447a5c35
angel <angel.garcia.gp@gmail.com>
2020-11-14 19:37:25 -0600

    Se agregaron los conroladores

--- src/main/java/gt/edu/umg/desarrollo/proyecto/Controller/SrvInventario.java
+++ src/main/java/gt/edu/umg/desarrollo/proyecto/Controller/SrvInventario.java
@@ -1,8 +1,9 @@
+
 package gt.edu.umg.desarrollo.proyecto.Controller;
 
-import gt.edu.umg.desarrollo.proyecto.Model.IPacienteDao;
-import gt.edu.umg.desarrollo.proyecto.Model.PacienteDao;
-import gt.edu.umg.desarrollo.proyecto.Model.PacienteEntity;
+import gt.edu.umg.desarrollo.proyecto.Model.IInventoryDao;
+import gt.edu.umg.desarrollo.proyecto.Model.InventoryDao;
+import gt.edu.umg.desarrollo.proyecto.Model.MedicineEntity;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -10,8 +11,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class SrvPaciente extends HttpServlet {
+/**
+ *
+ * @author Usuario
+ */
+public class SrvInventario extends HttpServlet {
 
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -20,31 +34,28 @@ public class SrvPaciente extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet SrvPaciente</title>");
+            out.println("<title>Servlet SrvInventario</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Pacientes Controller " + request.getContextPath() + "</h1>");
-
-            PacienteEntity paciente = new PacienteEntity();
-
-            IPacienteDao dao = new PacienteDao();
-
-            paciente.setIdPaciente(Integer.valueOf(request.getParameter("txt_id")));
-            paciente.setCarne(request.getParameter("txt_carne"));
-            paciente.setNombre(request.getParameter("txt_nombres"));
-            paciente.setDpi(Long.valueOf(request.getParameter("txt_dpi")));
-            paciente.setTelefono(Integer.valueOf(request.getParameter("txt_celpaciente")));
-            paciente.setTlefonoFamiliar(Integer.valueOf(request.getParameter("txt_celfamiliar")));
-            paciente.setDireccion(request.getParameter("txt_direccion"));
-            paciente.setFechaNacimiento(request.getParameter("txt_fecha"));
-
+            out.println("<h1>Servlet SrvInventario at " + request.getContextPath() + "</h1>");
+            
+            MedicineEntity medicina = new MedicineEntity();
+            IInventoryDao dao = new InventoryDao();
+            
+            medicina.setIdInventory(Integer.valueOf(request.getParameter("txt_id")));
+            medicina.setIdMedicine(Integer.valueOf(request.getParameter("drop_medicina")));
+            medicina.setProveedor(request.getParameter("txt_proveedor"));
+            medicina.setCantidad(Integer.valueOf(request.getParameter("txt_existencias")));
+            medicina.setFechaVencimiento(request.getParameter("txt_fn"));
+            
+            
             //Botno Guardar
             if ("agregar".equals(request.getParameter("btn_agregar"))) {
 
-                int contador = dao.AddPaciente(paciente);
-
+                int contador = dao.AddMedicine(medicina);
+                
                 if (contador > 0) {
-                    response.sendRedirect("pacientes.jsp");
+                    response.sendRedirect("inventario.jsp");
 
                 } else {
                     out.println("<h1> xxxxxxx No se Ingreso xxxxxxxxxxxx </h1>");
@@ -52,41 +63,11 @@ public class SrvPaciente extends HttpServlet {
                 }
             }
             
-             // ----------------------------------------------------
-           
-             // Boton modificar 
-            if ("modificar".equals(request.getParameter("btn_modificar"))){
-                
-                int contador = dao.UpdatePatient(paciente);
-                
-             if (contador>0){
-             response.sendRedirect("pacientes.jsp");
-             
-             }else{
-             out.println("<h1> xxxxxxx No se Modifico xxxxxxxxxxxx </h1>");
-             out.println("<a href='index.jsp'>Regresar...</a>");
-             }
-             }
+            
+            out.println(request.getParameter("txt_id"));
+            out.println(request.getParameter("drop_medicina"));
             
             
-            
-            // ----------------------------------------------------
-           
-           
-            // Boton eliminar 
-            if ("eliminar".equals(request.getParameter("btn_eliminar"))){
-                 
-                int contadorE = dao.DeletePatient(Integer.valueOf(request.getParameter("txt_id")));
-                 
-             if (contadorE>0){
-             response.sendRedirect("pacientes.jsp");
-             
-             }else{
-             out.println("<h1> xxxxxxx No se Elimino xxxxxxxxxxxx </h1>");
-             out.println("<a href='index.jsp'>Regresar...</a>");
-             }
-             }
-
             out.println("</body>");
             out.println("</html>");
         }
